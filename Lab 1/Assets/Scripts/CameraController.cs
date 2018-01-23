97e1348a23e37080845227b2bb39440f3720d889
troyfernandes <troyfernandes27@gmail.com>
2018-01-23 18:52:32 -0500

    Lab 1 Completed

---
+++ Lab 1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+
+
+    }
+
+    //void FixedUpdate() {
+    //    transform.position = player.transform.position;
+    //    transform.rotation = Quaternion.Euler(0f, player.transform.rotation.y, 0f);
+    //}
+}
---
+++ Lab 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    private int count;
+    public Text countText;
+    public float timer;
+    public Text winText;
+    public Text timerText;
+    public Camera FPScamera;
+    public Camera TopViewCamera;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+        if (!(count >= 4)) {
+            timer += Time.deltaTime;
+            setTimerText();
+        }
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 4)
+        {
+            winText.text = "You win";
+        }
+    }
+
+    void setTimerText() {
+        //timerText.text = "Time: " + timer.ToString() ;
+
+
+        int minutes = Mathf.FloorToInt(timer / 60F);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        string time = string.Format("{0:0}:{1:00}", minutes, seconds);
+        timerText.text = time;
+    }
+}
