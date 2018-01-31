5d07393141d60acc562d1321271c9dddbabf1ac5
j2ventre <j2ventre@ryerson.ca>
2018-01-31 13:36:03 -0500

    Lab 2 Progress

---
+++ Lab 2/Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInput : MonoBehaviour {
+    public SteamVR_TrackedObject mTrackedObject = null;
+    public SteamVR_Controller.Device mDevice;
+    public Rigidbody balloon;
+    public Transform controllerEnd;
+
+
+    public int Yvalue = 0;
+	// Use this for initialization
+	void Start () {
+        mTrackedObject = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mDevice = SteamVR_Controller.Input((int)mTrackedObject.index);
+
+        if (mDevice.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) {
+            print("Trigger Down");
+            Debug.Log("Trigger Down");
+
+            Rigidbody balloonInstance;
+
+            //Vector3 position = controllerEnd.position;
+            //position.y = position.y + Yvalue;
+
+            balloonInstance = Instantiate(balloon, controllerEnd.position, controllerEnd.rotation) as Rigidbody;
+            balloonInstance.AddForce(controllerEnd.up * 1);
+            Instantiate(balloon);
+
+        }
+        if (mDevice.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger)) {
+            print("Trigger up");
+            Debug.Log("Trigger up");
+            Instantiate(balloon);
+
+        }
+    }
+}
--- Lab 2/Assets/Scripts/ControllerScript.cs
+++ Lab 2/Assets/Scripts/ControllerScript.cs
@@ -6,14 +6,18 @@ public class ControllerScript : MonoBehaviour {
     private SteamVR_TrackedController device;
 	// Use this for initialization
 	void Start () {
-        Debug.Log("Debugging");
-        device = GetComponent<SteamVR_TrackedController>();
-        device.TriggerClicked += Trigger;
+        //Debug.Log("Debugging");
+        //device = GetComponent<SteamVR_TrackedController>();
+        //device.TriggerClicked += Trigger;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log("Debugging");
+        //if (ViveInput.GetPressDown(HandRole.RightHand, ControllerButton.Trigger)) {
+         //   Debug.Log("Trigger Pressed");
+
+        //}
+
     }
     void Trigger(object sender, ClickedEventArgs e) {
         Debug.Log("Trigger Pressed");
