e4921a386a36bc660496017f6508c3692f0227ba
Andrew <a264chan@scs.ryerson.ca>
2018-02-12 11:32:10 -0500

    Lab 4 Progress

---
+++ Lab 4/Assets/Scripts/Laser.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    LineRenderer lineRend;
+    Vector3 startPos = new Vector3(0, 0, 0);
+    Vector3 endPos = new Vector3(0, 0, 10);
+    RaycastHit hit;
+    public Selectable selectScript;
+    
+
+    void Start()
+    {
+        lineRend = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+
+        lineRend.SetPosition(0, startPos);
+        lineRend.SetPosition(1, endPos);
+        Selectable selectableObj = GetComponent<Selectable>();
+        if (Physics.Raycast(transform.position, transform.forward * 10, out hit)) {
+            if (hit.transform.tag == "selectable" && selectableObj != null) {
+                selectableObj.OutlineEnable();
+            }
+            else {
+                //selectableObj.OutlineDisable();
+            }
+        }
+    }
+}
---
+++ Lab 4/Assets/Scripts/Selectable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Selectable : MonoBehaviour {
+    Material[] materials;
+    public Material outlineMaterial;
+    //public void Remove()
+    // {
+    //  Destroy(gameObject);
+    //}
+    void Start() {
+        materials = GetComponent<Renderer>().materials;
+        gameObject.GetComponent<Renderer>().material = null;
+
+    }
+
+    public void OutlineEnable() {
+        gameObject.GetComponent<Renderer>().material = outlineMaterial;
+
+    }
+    public void OutlineDisable() {
+        gameObject.GetComponent<Renderer>().material = null;
+    }
+}
